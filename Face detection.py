8d3ea3511718f5d53fb0ab99072e3ad4ee10b7ce
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2023-06-03 01:34:02 +0600

    face detection

--- Face detection.py
+++ Face detection.py
@@ -1,29 +1,14 @@
 import cv2
-
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# To capture video from webcam.
-cap = cv2.VideoCapture(0)
-# To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
-
+face_cap=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+video= cv2.VideoCapture(0)
 while True:
-    # Read the frame
-    _, img = cap.read()
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 2)
+  _,video_frame=video.read()
+  changed_color=cv2.cvtColor(video_frame,cv2.COLOR_BGR2GRAY)
+  face_detect=face_cap.detectMultiScale(changed_color,1.1,5)
+  for (x,y,w,h) in face_detect:
+    cv2.rectangle(video_frame,(x,y),(x+w,y+h),(255, 0, 255), 2)
+  cv2.imshow("face detection by sad",video_frame)
+  if cv2.waitKey(10)==27:
+    break
 
-    # Display
-    cv2.imshow('img', img)
-    # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
-    if k==27:
-        break
-# Release the VideoCapture object
-cap.release()
+video.release()
